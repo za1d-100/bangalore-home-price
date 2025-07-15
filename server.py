e4db8d8790149f3b8ed8f9f7c11f3b787ca53e92
Zaid Ansari <zaidansari4910@gmail.com>
2025-07-15 13:57:56 +0530

    Added frontend and fixed route

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
-from flask import Flask,request, jsonify
+from flask import Flask,request, jsonify,render_template
 import util
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('app.html')  # or index.html
 
 @app.route('/get_location_names')
 def get_location_names():
